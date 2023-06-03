2ef289470436d3e4c240526bed8a7848c62dc7dd
Rafael Alfaro <rafalfaro_18@outlook.com>
2023-06-03 15:54:41 -0600

    Gets Environments Bundles via addressables and logs bundle name

---
+++ Assets/BundledScripts/EnvironmentsLoader.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace MetaverseSandbox {
+
+    [Serializable]
+    public class EnvironmentsReference : AssetReferenceT<Environments>
+    {
+        public EnvironmentsReference(string guid) : base(guid)
+        {
+        }
+    }
+    public class EnvironmentsLoader : MonoBehaviour
+    {
+        public List<EnvironmentsReference> environmentBundles;
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (environmentBundles == null) { return; }
+
+            if (environmentBundles.Count == 0) { return; }
+
+            var Testenv = environmentBundles[0].LoadAssetAsync();
+            Testenv.Completed += Testenv_Completed;
+
+        }
+
+        private void Testenv_Completed(UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<Environments> obj)
+        {
+            if (obj.Status == UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationStatus.Succeeded)
+            {
+                Debug.Log(obj.Result.name); // Gets the Environments Bundles
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
